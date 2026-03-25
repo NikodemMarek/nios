33027d176522814b9739af254b6a0d22fcfa2dd2
NikodemMarek <62289991+NikodemMarek@users.noreply.github.com>
2026-03-25 13:52:35 +0100

    feat: trap entrypoint and trap handler scaffold

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,7 @@
 use core::arch::global_asm;
 use core::panic::PanicInfo;
 
-global_asm!(
-    ".section .text.boot",
-    ".global _start",
-    "_start:",
-    "    la sp, 0x88000000",
-    "    call kernel_main",
-    "1:  wfi",
-    "    j 1b",
-);
+global_asm!(include_str!("trap.s"));
 
 const UART: *mut u8 = 0x10000000 as *mut u8;
 
@@ -27,7 +19,19 @@ fn uart_print(s: &str) {
 #[unsafe(no_mangle)]
 pub extern "C" fn kernel_main() -> ! {
     uart_print("Hello from nios!\n");
+    unsafe { core::arch::asm!("ecall") };
     loop {}
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn trap_handler(machine_cause: u64) -> () {
+    if machine_cause == 8 {
+        uart_print("ecall trap called!\n");
+        todo!("handle ecall")
+    } else {
+        uart_print("error trap called!\n");
+        todo!("handle error")
+    }
 }
 
 #[panic_handler]
---
+++ src/trap.s
@@ -0,0 +1,91 @@
+.section .text.boot
+.global _start
+_start:
+    // specify entrypoint of the os
+    la sp, 0x88000000
+
+    // specify the trap handler entry point
+    la t0, trap_entry
+    csrw mtvec, t0
+
+    // start the kernel
+    call kernel_main
+
+// loop forever in case something goes wrong
+1:  wfi
+    j 1b
+
+.align 4
+trap_entry:
+    // store registers in the trap frame
+    addi sp, sp, -248
+    sd x1,  0(sp)
+    sd x2,  8(sp)
+    sd x3,  16(sp)
+    sd x4,  24(sp)
+    sd x5,  32(sp)
+    sd x6,  40(sp)
+    sd x7,  48(sp)
+    sd x8,  56(sp)
+    sd x9,  64(sp)
+    sd x10, 72(sp)
+    sd x11, 80(sp)
+    sd x12, 88(sp)
+    sd x13, 96(sp)
+    sd x14, 104(sp)
+    sd x15, 112(sp)
+    sd x16, 120(sp)
+    sd x17, 128(sp)
+    sd x18, 136(sp)
+    sd x19, 144(sp)
+    sd x20, 152(sp)
+    sd x21, 160(sp)
+    sd x22, 168(sp)
+    sd x23, 176(sp)
+    sd x24, 184(sp)
+    sd x25, 192(sp)
+    sd x26, 200(sp)
+    sd x27, 208(sp)
+    sd x28, 216(sp)
+    sd x29, 224(sp)
+    sd x30, 232(sp)
+    sd x31, 240(sp)
+
+    // move the machine cause to function argument, and call the trap handler
+    csrr x10, mcause
+    call trap_handler
+
+    // restore registers
+    ld x1,  0(sp)
+    ld x2,  8(sp)
+    ld x3,  16(sp)
+    ld x4,  24(sp)
+    ld x5,  32(sp)
+    ld x6,  40(sp)
+    ld x7,  48(sp)
+    ld x8,  56(sp)
+    ld x9,  64(sp)
+    ld x10, 72(sp)
+    ld x11, 80(sp)
+    ld x12, 88(sp)
+    ld x13, 96(sp)
+    ld x14, 104(sp)
+    ld x15, 112(sp)
+    ld x16, 120(sp)
+    ld x17, 128(sp)
+    ld x18, 136(sp)
+    ld x19, 144(sp)
+    ld x20, 152(sp)
+    ld x21, 160(sp)
+    ld x22, 168(sp)
+    ld x23, 176(sp)
+    ld x24, 184(sp)
+    ld x25, 192(sp)
+    ld x26, 200(sp)
+    ld x27, 208(sp)
+    ld x28, 216(sp)
+    ld x29, 224(sp)
+    ld x30, 232(sp)
+    ld x31, 240(sp)
+    addi sp, sp, 248
+    mret
