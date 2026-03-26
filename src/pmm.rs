6da34cb6bf90bd4c028e3881552e0341ae73016c
NikodemMarek <62289991+NikodemMarek@users.noreply.github.com>
2026-03-26 12:37:33 +0100

    feat: free reserved pages

--- src/pmm.rs
+++ src/pmm.rs
@@ -3,6 +3,7 @@ const SIZE: u64 = 128 * 1024 * 1024;
 const PAGE_SIZE: u64 = 4096;
 
 struct Page {
+    index: u64,
     start: u64,
 }
 
@@ -14,17 +15,26 @@ impl<'a> Pmm<'a> {
         for (n, sector) in self.bitmap.iter_mut().enumerate() {
             for i in 0..64 {
                 if (*sector >> i) & 0b1 == 0 {
-                    let start = START + ((n as u64 * 64) + i) * PAGE_SIZE;
+                    let index = (n as u64 * 64) + i;
+                    let start = START + (index) * PAGE_SIZE;
+
                     if start > START + SIZE {
                         panic!("No free pages");
                     }
 
                     *sector |= 0b1 << i;
-                    return Page { start };
+                    return Page { index, start };
                 }
             }
         }
 
         panic!("No free pages");
+    }
+
+    pub fn free(&mut self, index: u64) {
+        let n = index / 64;
+        let i = index % 64;
+
+        *self.bitmap.get_mut(n as usize).unwrap() ^= 0b1 << i;
     }
 }
