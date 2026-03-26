cd1afc6f176a733f08202b0d0ef7fd4e1dea5369
NikodemMarek <62289991+NikodemMarek@users.noreply.github.com>
2026-03-26 14:07:15 +0100

    feat: better alloc and free

--- src/pmm.rs
+++ src/pmm.rs
@@ -1,4 +1,8 @@
-const START: u64 = 0x80000000;
+unsafe extern "C" {
+    static _kernel_start: u8;
+    static _kernel_end: u8;
+}
+
 const SIZE: u64 = 128 * 1024 * 1024;
 const PAGE_SIZE: u64 = 4096;
 
@@ -7,22 +11,35 @@ struct Page {
     start: u64,
 }
 
-struct Pmm<'a> {
-    bitmap: &'a mut [u64],
+struct Pmm {
+    bitmap: *mut u64,
+    len: u64,
 }
-impl<'a> Pmm<'a> {
-    pub fn first(&mut self) -> Page {
-        for (n, sector) in self.bitmap.iter_mut().enumerate() {
-            for i in 0..64 {
-                if (*sector >> i) & 0b1 == 0 {
-                    let index = (n as u64 * 64) + i;
-                    let start = START + (index) * PAGE_SIZE;
+impl Pmm {
+    pub fn new() -> Pmm {
+        let bitmap_start = (&raw const _kernel_end as u64) as *mut u64;
+        Pmm {
+            bitmap: bitmap_start,
+            len: SIZE / PAGE_SIZE / 64,
+        }
+    }
 
-                    if start > START + SIZE {
+    pub fn alloc(&mut self) -> Page {
+        for n in 0..self.len {
+            for i in 0..64 {
+                let sector_ptr = unsafe { self.bitmap.add(n as usize) };
+                let sector = unsafe { *sector_ptr };
+
+                if (sector >> i) & 0b1 == 0 {
+                    let index = (n * 64) + i;
+                    let kernel_end = &raw const _kernel_end as u64;
+                    let start = kernel_end + index * PAGE_SIZE;
+
+                    if start > kernel_end + SIZE {
                         panic!("No free pages");
                     }
 
-                    *sector |= 0b1 << i;
+                    unsafe { *sector_ptr = sector | (0b1 << i) };
                     return Page { index, start };
                 }
             }
@@ -35,6 +52,8 @@ impl<'a> Pmm<'a> {
         let n = index / 64;
         let i = index % 64;
 
-        *self.bitmap.get_mut(n as usize).unwrap() ^= 0b1 << i;
+        let sector_ptr = unsafe { self.bitmap.add(n as usize) };
+        let sector = unsafe { *sector_ptr };
+        unsafe { *sector_ptr = sector & !(0b1 << i) };
     }
 }
