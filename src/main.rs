950c63cc6f0719a29f17847032941298b656c9ee
NikodemMarek <62289991+NikodemMarek@users.noreply.github.com>
2026-03-30 20:50:55 +0200

    feat!: add global allocator

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,60 @@ mod pmm;
 mod shell;
 mod uart;
 
+use core::alloc::GlobalAlloc;
 use core::arch::global_asm;
+use core::cell::{RefCell, RefMut};
 use core::fmt::Write;
 use core::panic::PanicInfo;
 
+use crate::heap::Heap;
 use crate::uart::Uart;
 
 global_asm!(include_str!("main.s"));
 
+#[global_allocator]
+static ALLOCATOR: GlobalAllocator = GlobalAllocator::empty();
+
 #[unsafe(no_mangle)]
 pub extern "C" fn kernel_main() -> ! {
     let pmm = pmm::Pmm::new();
-    let mut heap = heap::Heap::new(pmm);
+    let heap = Heap::new(pmm);
 
-    shell::run(&mut heap);
+    {
+        let mut allocator = ALLOCATOR.0.borrow_mut();
+        *allocator = Some(heap);
+    }
 
     loop {}
 }
+
+pub struct GlobalAllocator(RefCell<Option<Heap>>);
+impl GlobalAllocator {
+    #[inline]
+    const fn empty() -> Self {
+        GlobalAllocator(RefCell::new(None))
+    }
+
+    #[inline]
+    fn get(&self) -> core::cell::RefMut<'_, Heap> {
+        RefMut::map(self.0.borrow_mut(), |mi| {
+            mi.as_mut().expect("Allocator not initialized")
+        })
+    }
+}
+unsafe impl GlobalAlloc for GlobalAllocator {
+    #[inline]
+    unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
+        self.get().malloc(layout.size() as u64) as *mut u8
+    }
+
+    #[inline]
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {
+        self.get().free(ptr);
+    }
+}
+unsafe impl Send for GlobalAllocator {}
+unsafe impl Sync for GlobalAllocator {}
 
 #[unsafe(no_mangle)]
 pub extern "C" fn trap_handler(machine_cause: u32) {
